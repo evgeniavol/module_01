0216da26fcb20175bcdd300e9fd88bee9dcc3360
Evgenia <evgeniavolkova120@gmail.com>
2021-11-20 19:31:47 +0300

    lesson01

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const nameProduct = 'chairs';
+const amountProduct = 20;
+const categoryProduct = 'furniture';
+const priceProduct = 1500;
+console.log('наименование товара: ', nameProduct);
