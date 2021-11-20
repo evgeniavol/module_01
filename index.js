384004fd53fa959b36baced39248e8c49a132513
Evgenia <evgeniavolkova120@gmail.com>
2021-11-20 19:41:25 +0300

    added a second product

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-"use strict";
+"use strict"; {
+    const nameProduct = 'chairs';
+    const amountProduct = 20;
+    const categoryProduct = 'furniture';
+    const priceProduct = 1500;
+    console.log(`наименование товара: ${nameProduct}`);
+    console.log(`общая сумма товара: ${amountProduct * priceProduct}`);
+}
 
-const nameProduct = 'chairs';
-const amountProduct = 20;
-const categoryProduct = 'furniture';
-const priceProduct = 1500;
-console.log('наименование товара: ', nameProduct);
+{
+    const nameProduct = 'tables';
+    const amountProduct = 30;
+    const categoryProduct = 'furniture';
+    const priceProduct = 9000;
+    console.log(`наименование товара: ${nameProduct}`);
+    console.log(`общая сумма товара: ${amountProduct * priceProduct}`);
+}
